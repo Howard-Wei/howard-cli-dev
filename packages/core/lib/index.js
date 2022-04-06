7416a9dc746821451051d868eef35686944b9de2
howardwei <howardwei@tencent.com>
2022-04-06 14:47:12 +0800

    feat(package-init): 项目初始化

---
+++ packages/core/lib/index.js
@@ -0,0 +1,14 @@
+const utils = require('@howard-cli-dev/utils')
+const argvs = require('process').argv
+console.log('🚀 ~ file: index.js ~ line 3 ~ argvs', argvs)
+const command = argvs[2]
+const [option, value] = argvs.slice(2)
+if (command) {
+  if (utils[command]) {
+    utils[command](option, value)
+  } else {
+    console.error('command not found')
+  }
+} else {
+  console.error('no command')
+}
---
+++ packages/utils/lib/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+module.exports = {
+  init (option, value) {
+    console.log('-----init start-----')
+    console.log('🚀 ~ file: index.js ~ line 5 ~ init ~ value', value)
+    console.log('🚀 ~ file: index.js ~ line 5 ~ init ~ option', option)
+    console.log('-----init end-----')
+  },
+  version () {
+    console.log('v1.0.0')
+  }
+}
