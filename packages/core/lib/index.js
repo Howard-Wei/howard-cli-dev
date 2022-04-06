2eb8d19dd6605178e3daef1e4f15ff0783865dd1
howardwei <howardwei@tencent.com>
2022-04-06 15:05:35 +0800

    feat(new console): 新增话术

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -3,6 +3,7 @@ const argvs = require('process').argv
 console.log('🚀 ~ file: index.js ~ line 3 ~ argvs', argvs)
 const command = argvs[2]
 const [option, value] = argvs.slice(2)
+
 if (command) {
   if (utils[command]) {
     utils[command](option, value)
--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   init (option, value) {
+    console.log('-----welcome to howard cli-----')
     console.log('-----init start-----')
     console.log('🚀 ~ file: index.js ~ line 5 ~ init ~ value', value)
     console.log('🚀 ~ file: index.js ~ line 5 ~ init ~ option', option)
